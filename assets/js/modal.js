3818ffd4472aedbfcec7f367c964df59b20e5842
LeoHenriques0 <alfredoalbertohenriques@gmail.com>
2024-11-05 18:54:44 +0100

    Acabamos de estilizar a Home e alteramos um pouco o HTML e também uma JS no formato da modal.Não terminamos ainda de estilizar as modals até aqui tudo bem

---
+++ assets/js/modal.js
@@ -0,0 +1,19 @@
+//HIDE N SHOW OF HEADER -----------------------------------------------------------------------------------------------------------------------
+function informaction() {
+    var x = document.getElementById("infomore");
+    if(x.style.display == "none") {
+               x.style.display = "block";
+           }
+        else {
+               x.style.display = "none";
+    }
+}
+function helper() {
+    var x = document.getElementById("contact");
+    if(x.style.display == "none") {
+               x.style.display = "block";
+           }
+        else {
+               x.style.display = "none";
+    }
+}
